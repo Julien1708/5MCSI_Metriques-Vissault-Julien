4e93cf0225567d07654672bda332c4f02c39937d
Julien1708 <julien.vissault@dxc.com>
2025-12-03 10:30:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ def mongraphique():
 
 @app.route('/histogramme/')
 def meteo_graph():
-    return render_template('histogramme.html')
+    return render_template("histogramme.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
